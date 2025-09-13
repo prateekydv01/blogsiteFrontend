d401ba0f70e937dc7d17afad525ef7789da7022f
prateekydv01 <prateekyadav2023@gmail.com>
2025-09-14 00:41:45 +0530

    footer css file romeved

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../logo'
-import './footer.css'
+
 
 function Footer() {
   const currentYear = new Date().getFullYear()
